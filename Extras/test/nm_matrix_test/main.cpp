bbd3658975f116a20f568c26911015f790238ed2
NatanMeirov <NatanMeirov@gmail.com>
2021-12-16 00:44:46 +0200

    NM CT Matrix: Added Makefile and Unittest framework, to start testing the NM Matrix code

---
+++ Extras/test/nm_matrix_test/main.cpp
@@ -0,0 +1,15 @@
+// #include "mu_test.h"
+// #include "nm_matrix.hpp"
+#include "../inc/mu_test.h" //! remove
+#include "../../nm_matrix_single_header/nm_matrix.hpp" //! remove
+
+
+// TODO: write Unit tests!
+
+
+
+TEST_SUITE(NM_Compile_Time_Matrix_Test)
+
+// TODO: add tests
+
+END_SUITE
