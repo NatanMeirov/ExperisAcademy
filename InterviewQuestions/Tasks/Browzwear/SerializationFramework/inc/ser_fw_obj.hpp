22a2a700cb2d12771ae894d084e61189d2711cb1
Natan Meirov <NatanMeirov@gmail.com>
2021-12-23 23:44:43 +0200

    SerFW: Another update of the documentation of SerFwObj, and renaming of: SerFwObj -> ISerFwObj

--- InterviewQuestions/Tasks/Browzwear/SerializationFramework/inc/ser_fw_obj.hpp
+++ InterviewQuestions/Tasks/Browzwear/SerializationFramework/inc/ser_fw_obj.hpp
@@ -1,10 +1,10 @@
-#ifndef SF_SER_FW_OBJ_HPP
-#define SF_SER_FW_OBJ_HPP
+#ifndef SF_ISER_FW_OBJ_HPP
+#define SF_ISER_FW_OBJ_HPP
 
 
 #include <string>
 #include <memory> // std::shared_ptr
-#include "ser_fw_types.hpp"
+#include "iser_fw_types.hpp"
 #include "itransformable.hpp"
 #include "iserialization_formatter.hpp"
 #include "ideserialization_formatter.hpp"
@@ -16,20 +16,21 @@ namespace ser_fw
 namespace infra
 {
 
-// The base class of all Serializeable/Deserializeable objects of the Serialization Framework.
-// Each derived CONCRETE class must be default-constructable, copy-constructable and copy-assignable
-// (to be used in a C++ STL container), and must implement: TypeName() as string (should be similar to its class name), TransformFrom() - from ParamsMap type
-// and TransformTo() - to ParamsMap type, to be able to be serialized/deserialized without any dependency on a concrete file/data format.
-// [The derived class should use insert() method to insert each key-value std::pair
-// in the implementation of TransformTo(), and should use at() method on each key, to extract its matched value in the implementation of TransformFrom()].
-// Also, each derived concrete class must implement a factory class that inherit from SerFwObjFactory
-// and implement Create method - that should be used to create the object within the Serialization Framework, using the Reflector class.
+// The root interface of all Serializeable/Deserializeable objects in the Serialization Framework.
+// Each derived concrete class must be default-constructable, copy-constructable and copy-assignable
+// (to be used in a C++ STL container), and must implement: TypeName() - that returns string that should be similar to its class name, TransformFrom() - ParamsMap type
+// into self, and TransformTo() - ParamsMap type from self.
+// All these are needed to be able to be serialized and deserialized without any dependency on a particular file/data format.
+// [Note: The derived class should use insert() method to insert each key-value std::pair - in the implementation of TransformTo(),
+// and should use at() method on each key to extract its matched value - in the implementation of TransformFrom()].
+// Also, each derived concrete class must implement a factory class that inherit from ISerFwObjFactory
+// and implement the Create() method - that should be used to create the object within the Serialization Framework, using the Reflector class.
 // ParamsMap type: represents a {param:value} mapping (stored as {string:string} unordered_map) -
-// while each key is a specific concrete param name, and the value is its matched concrete param's value.
-class SerFwObj : public infra::ITransformable<Types::ParamsMap>
+// while each key is a specific concrete param name, and the value is its matched concrete value.
+class ISerFwObj : public infra::ITransformable<Types::ParamsMap>
 {
 public:
-    virtual ~SerFwObj() = default;
+    virtual ~ISerFwObj() = default;
     virtual std::string TypeName() const = 0;
 };
 
@@ -38,4 +39,4 @@ public:
 } // ser_fw
 
 
-#endif // SF_SER_FW_OBJ_HPP
+#endif // SF_ISER_FW_OBJ_HPP
